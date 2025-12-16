f93eb6797f16b743309c1e70e8e34312b7d38513
Koena Banerjee <koenabanerjee966@gmail.com>
2025-12-16 22:57:43 +0530

    revisiting js concepts

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2 //not allowed
+
+accountEmail = "xyz@gmail.com"
+accountPassword = "212345"
+accountCity = "Bengaluru"
+
+
+console.log(accountId);
+
+/* Prefer not to use var because of issue in block scope and functional scope*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) //We are using nodejs, not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+
+
+let name = "Koena"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value has not been defined yet
+// symbol => unique
+
+// object
+
+console.log(typeof "Koena");
+console.log(typeof null); // output is object
+console.log(typeof undefined); //output is undefined
+
+//ECMA Standards - document that defines rules for programming languages (like js), ensuring products work together by setting common specifications.
