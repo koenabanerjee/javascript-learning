5f02d5529ba888d483a23c076cdbb71cdd84c8e9
Koena Banerjee <koenabanerjee966@gmail.com>
2025-12-19 00:42:44 +0530

    commits

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -18,7 +18,7 @@ console.log(typeof(bigNumber))
 
 //JavaScript is a dynamically typed language. This means that the type of a variable is determined at runtime by the value it holds, rather than being explicitly declared and fixed at compile time. 
 
-//Reference (Non - primitive)
+//Reference (Non - primitive) (call by reference)
 // Array, Objects, Functions
 
 const heroes = ["shaktiman", "naagraj", "doga"]
@@ -35,3 +35,30 @@ console.log(typeof heroes); //object
 console.log(typeof myObj); //object
 console.log(typeof myFunction); //function
 console.log(typeof id); //symbol
+
+
+// +++++++++++++++++++++++++++++++++++++++++++
+
+//Stack(Primitive) , Heap(Non-Primitive)
+
+let myName = "koenabanerjee"
+let anothername = myName
+anothername = "bla"
+
+console.log(myName);
+console.log(anothername);
+
+let user1 = {
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+let user2 = user1
+
+user2.email = "koena@google.com"
+
+console.log(user1.email); //koena@google.com
+console.log(user2.email); //koena@google.com
+
+//PRIMITIVE -> STACK -> CALL BY VALUE -> CHANGES ARE DONE IN THE COPY AND NOT IN THE ORIGINAL VALUE
+// NON PRIMITIVE -> HEAP -> REFERENCE -> CHANGES ARE DONE IN ORIGINAL VALUES
